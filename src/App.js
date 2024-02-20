a29b543ca04ecc4876339da6da7d271004036f75
Vibhor <vibhorsingla3104@gmail.com>
2024-02-20 11:19:48 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import AddEmployee from "./components/AddEmployee";
+import Navbar from "./components/Navbar";
+import EmployeeList from "./components/EmployeeList";
+import UpdateEmployee from "./components/UpdateEmployee";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Navbar /> {/* this we need to show everytime */}
+        <Routes>
+          <Route index element={<EmployeeList />} />
+          <Route path="/" element={<EmployeeList />} />
+          <Route path="/employeeList" element={<EmployeeList />} />
+          <Route path="/addEmployee" element={<AddEmployee />} />
+          <Route path="/editEmployee/:id" element={<UpdateEmployee />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
